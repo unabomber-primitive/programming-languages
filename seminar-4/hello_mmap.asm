8e665f3c271253bbc523803b1a58c62945c6d10f
Andrey Zhdanov <adzhdanov@itmo.ru>
2022-10-12 18:48:03 +0000

    Update seminar-4/hello_mmap.asm, seminar-4/hello.txt files

--- seminar-4/hello_mmap.asm
+++ seminar-4/hello_mmap.asm
@@ -2,56 +2,73 @@
 %define O_RDONLY 0 
 %define PROT_READ 0x1
 %define MAP_PRIVATE 0x2
+%define SYS_WRITE 1
 %define SYS_OPEN 2
 %define SYS_MMAP 9
+%define FD_STDOUT 1
 
 section .data
-; This is the file name. You are free to change it.
-fname: db 'hello.txt', 0
+    ; This is the file name. You are free to change it.
+    fname: db 'hello.txt', 0
 
 section .text
 global _start
+
+; use exit system call to shut down correctly
 exit:
-  mov rax, 60           ; use exit system call to shut down correctly
-  xor rdi, rdi
-  syscall
+    mov  rax, 60
+    xor  rdi, rdi
+    syscall
+
 ; These functions are used to print a null terminated string
+; rdi holds a string pointer
 print_string:
     push rdi
     call string_length
-    pop rsi
-    mov rdx, rax 
-    mov rax, 1
-    mov rdi, 1 
+    pop  rsi
+    mov  rdx, rax 
+    mov  rax, SYS_WRITE
+    mov  rdi, FD_STDOUT
     syscall
     ret
+
 string_length:
-    xor rax, rax
+    xor  rax, rax
 .loop:
-    cmp byte [rdi+rax], 0
-    je .end 
-    inc rax
+    cmp  byte [rdi+rax], 0
+    je   .end 
+    inc  rax
     jmp .loop 
 .end:
     ret
 
+; This function is used to print a substring with given length
+; rdi holds a string pointer
+; rsi holds a substring length
+print_substring:
+    mov  rax, SYS_WRITE
+    mov  rdi, FD_STDOUT
+    mov  rdx, rsi 
+    mov  rsi, rdi
+    syscall
+    ret
+
 _start:
-
-; Вызовите open и откройте fname в режиме read only.
-mov rax, SYS_OPEN
-mov rdi, fname
-mov rsi, O_RDONLY    ; Open file read only
-mov rdx, 0 	         ; We are not creating a file
+    ; Вызовите open и откройте fname в режиме read only.
+    mov  rax, SYS_OPEN
+    mov  rdi, fname
+    mov  rsi, O_RDONLY    ; Open file read only
+    mov  rdx, 0 	         ; We are not creating a file
                       ; so this argument has no meaning
-syscall
+    syscall
 
-; Вызовите mmap c правильными аргументами
-; Дайте операционной системе самой выбрать, куда отобразить файл
-; Размер области возьмите в размер страницы 
-; Область не должна быть общей для нескольких процессов 
-;  и должна выделяться только для чтения.
+    ; Вызовите mmap c правильными аргументами
+    ; Дайте операционной системе самой выбрать, куда отобразить файл
+    ; Размер области возьмите в размер страницы 
+    ; Область не должна быть общей для нескольких процессов 
+    ; и должна выделяться только для чтения.
 
 
-; с помощью print_string теперь можно вывести его содержимое
+    ; с помощью print_string теперь можно вывести его содержимое
 
-call exit
+    call exit
